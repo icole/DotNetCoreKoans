cdff3ccec01003a5c84dde9bcb8b51753f124fd1
Ian Cole <ian.cole@me.com>
2020-11-27 02:14:11 -0800

    Finsh remaining koans

--- Koans/AboutArrays.cs
+++ Koans/AboutArrays.cs
@@ -77,10 +77,10 @@ namespace DotNetCoreKoans.Koans
             var array = new[] { 1, 2 };
             var stack = new Stack(array);
             stack.Push("last");
-            Assert.Equal(FILL_ME_IN, stack.ToArray());
+            Assert.Equal(new Object[] { "last", 2, 1 }, stack.ToArray());
             var poppedValue = stack.Pop();
-            Assert.Equal(FILL_ME_IN, poppedValue);
-            Assert.Equal(FILL_ME_IN, stack.ToArray());
+            Assert.Equal("last", poppedValue);
+            Assert.Equal(new Object[] { 2, 1 }, stack.ToArray());
         }
 
         [Step(6)]
@@ -94,16 +94,16 @@ namespace DotNetCoreKoans.Koans
             var list = new LinkedList<string>(array);
 
             list.AddFirst("Say");
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            Assert.Equal(new[] { "Say", "Hello", "World" }, list.ToArray());
 
             list.RemoveLast();
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            Assert.Equal(new[] { "Say", "Hello" }, list.ToArray());
 
             list.RemoveFirst();
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            Assert.Equal(new[] { "Hello" }, list.ToArray());
 
             list.AddAfter(list.Find("Hello"), "World");
-            Assert.Equal(FILL_ME_IN, list.ToArray());
+            Assert.Equal(new[] { "Hello", "World" }, list.ToArray());
         }
 
     }
